1fa56245c293947f9ab5346906eba5cd46c88418
thesamcode <stores1@digitalmail.one>
2022-08-06 18:29:59 -0700

    new updates

--- src/main/java/com/thesamecode/daikitchiroutes/DaikitchiController.java
+++ src/main/java/com/thesamecode/daikitchiroutes/DaikitchiController.java
@@ -1,24 +1,42 @@
 package com.thesamecode.daikitchiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/daikitchi")
 public class DaikitchiController {
 		
-	@RequestMapping("/daikitchi")
+	@RequestMapping("/")
 	public String index() {
 		return "Welcome!";
 	}
 	
-	@RequestMapping("/daikitchi/today")
+	@RequestMapping("/today")
 	public String index2() {
 		return "Today you will find good luck in all your endeavors!";
 	}
 	
-	@RequestMapping("/daikitchi/tomorrow")
+	@RequestMapping("/tomorrow")
 	public String index3() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	
+	@RequestMapping("/travel/{city}")
+    public String showCityWelcome(@PathVariable("city") String city){
+    	return "Congratulations! You will soon travel to " + city + "!";
+    }
+	
+	@RequestMapping("/lotto/{integer}")
+    public String showLesson(@PathVariable ("integer") int integer){
+		if (integer % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor, but now is a great time to spend time with family and friends.";
+		}
+    }
 
 }
+
